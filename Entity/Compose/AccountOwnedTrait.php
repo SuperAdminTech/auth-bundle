e1a95a8f1793fc3551c57749e825708bcbcf86f7
Lluis Santos <lluis@qbitartifacts.com>
2021-02-05 23:15:44 +0100

    Revert "fix(owned): added filters for user_id and account_id"
    This reverts commit d7482e5a

--- Entity/Compose/AccountOwnedTrait.php
+++ Entity/Compose/AccountOwnedTrait.php
@@ -6,13 +6,10 @@ use SuperAdmin\Bundle\Security\Account;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Serializer\Annotation\Groups;
 use Symfony\Component\Validator\Constraints as Assert;
-use ApiPlatform\Core\Annotation\ApiFilter;
-use ApiPlatform\Core\Bridge\Doctrine\Orm\Filter\SearchFilter;
 
 /**
  * Trait AccountOwnedTrait
  * @package SuperAdmin\Bundle\Entity\Compose
- * @ApiFilter(SearchFilter::class, properties={"account_id": "exact"})
  */
 trait AccountOwnedTrait {
 
